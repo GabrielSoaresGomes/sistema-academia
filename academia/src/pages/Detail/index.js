725469c59d812953826a48993e46bac35134af0f
Junior Nascimento <junior.nascimento@smartnx.com>
2023-09-26 15:41:41 -0300

    feat: adiciona carrosel para imagens dos exercicios

--- academia/src/pages/Detail/index.js
+++ academia/src/pages/Detail/index.js
@@ -20,6 +20,32 @@ const Detail = () => {
     return (
             <div className={'d-flex flex-column justify-content-center align-items-center gap-2'}>
                 <h1 className={'h1 text-center text-white'}>{exerciseData?.title}</h1>
+                <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                    <div className="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img className={'detail-img'} src={exerciseData?.thumbnail_url} alt="Thumbnail Image"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className={'detail-img'} src={exerciseData?.primary_image_url} alt="Primary Image"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className={'detail-img'} src={exerciseData?.secondary_image_url} alt="Secondary Image"/>
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
                 {
                     exerciseData?.description?.split('\n').map(value => {
                         if (value !== "" && value != null) {
@@ -27,8 +53,6 @@ const Detail = () => {
                         }
                     })
                 }
-                <img className={'detail-img'} src={exerciseData?.primary_image_url} alt="Primary Image"/>
-                <img className={'detail-img'} src={exerciseData?.secondary_image_url} alt="Secondary Image"/>
                 <div className="text-center">
                     <iframe width="853" height="480" src={exerciseData?.video_url} title={exerciseData?.title}
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
